457cd40399802e91bfd36fb0f6d8f668750efa07
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2023-06-07 16:56:06 +0200

    Update main.js

--- Weather App/main.js
+++ Weather App/main.js
@@ -1,1 +1,33 @@
 import './style.css'
+import { getWeather } from './weather'
+import { ICON_MAP } from './iconMap'
+
+getWeather(10.09, 10.09, Intl.DateTimeFormat().resolvedOptions().timeZone).then(renderWeather).catch(e=>{
+    console.error(e)
+    alert('Hups something went wrong on wether getting')
+})
+
+function renderWeather({current,daily,hourly}){
+    renderCurrentWeather(current)
+    // renderDailyWeather(daily)
+    // renderHourlyWeather(hourly)
+}
+
+
+function setValue(selector, value, { parent = document } = {}) {// function that is setting values to selectors
+    parent.querySelector(`[data-${selector}]`).textContent = value // same as this document.querySelector("[data-current-temp]").textContent = current.currentTemp
+}
+  
+  
+function getIconUrl(iconCode) {   
+    return `icons/${ICON_MAP.get(iconCode)}.svg`
+}
+  
+const currentIcon = document.querySelector("[data-current-icon]")
+    function renderCurrentWeather(current) {
+        currentIcon.src = getIconUrl(current.iconCode)
+        setValue("current-temp", current.currentTemp)
+        setValue("current-high", current.highTemp)
+        setValue("current-low", current.lowTemp)
+        setValue("current-wind", current.windSpeed)
+  }
