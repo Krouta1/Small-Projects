68345847ffbd1b3702c045553afd4b61de59fa62
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2023-06-08 20:41:21 +0200

    Create Prime number.js

---
+++ Algorithms/Prime number.js
@@ -0,0 +1,31 @@
+function isPrime (n) {
+    if ( n < 2 ){
+        return false
+    }
+    for ( let i = 2 ; i < n; i++){
+        if (n % i === 0) {
+            return false
+        }
+    }
+    return true
+    
+}// Big o is O(n)
+
+//  3 % 2 = 1 so it works
+//  4 % 2 = 0  works
+// 5 % 2 = 1; 5 % 3 = 2; 5 % 4 = 1 works
+
+// there is another solution 
+
+function isPrime2 (n) {
+    if ( n < 2 ){
+        return false
+    }
+    for ( let i = 2 ; i <= Math.sqrt(n); i++){
+        if (n % i === 0) {
+            return false
+        }
+    }
+    return true
+    
+}
