d7df0bd13d39dc8cabb630091351d8596ee72609
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2023-06-11 20:13:02 +0200

    Create App.js

---
+++ infinite-scrolling-app/src/App.js
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <input type="text"></input>
+        <div>Title</div>
+        <div>Title</div>
+        <div>Title</div>
+        <div>Title</div>
+        <div>Loading...</div>
+        <div>Error</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
