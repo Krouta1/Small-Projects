5a2e05a04a9855c4cb3c91990520bb19e108c0e3
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2023-06-12 20:45:50 +0200

    Update App.js

--- infinite-scrolling-app/src/App.js
+++ infinite-scrolling-app/src/App.js
@@ -1,18 +1,46 @@
+import React, { useState, useRef, useCallback } from 'react'
+import useBookSearch from './useBookSearch'
 
-function App() {
+export default function App() {
+  const [query, setQuery] = useState('')
+  const [pageNumber, setPageNumber] = useState(1)
+
+  const {
+    books,
+    hasMore,
+    loading,
+    error
+  } = useBookSearch(query, pageNumber)
+
+  const observer = useRef()
+  const lastBookElementRef = useCallback(node => { //callBack + useRef some handy trick :)
+    if (loading) return
+    if (observer.current) observer.current.disconnect()
+    observer.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting && hasMore) {
+        setPageNumber(prevPageNumber => prevPageNumber + 1) // paginating but only till it hasMore to paginate over try it on: The Lord of the rings
+      }
+    })
+    if (node) observer.current.observe(node)
+  }, [loading, hasMore])
+
+  function handleSearch(e) {
+    setQuery(e.target.value)
+    setPageNumber(1)
+  }
+
   return (
     <>
-      <div className="App">
-        <input type="text"></input>
-        <div>Title</div>
-        <div>Title</div>
-        <div>Title</div>
-        <div>Title</div>
-        <div>Loading...</div>
-        <div>Error</div>
-      </div>
+      <input type="text" value={query} onChange={handleSearch}></input>
+      {books.map((book, index) => {
+        if (books.length === index + 1) {
+          return <div ref={lastBookElementRef} key={book}>{book}</div> // very last book in books array
+        } else {
+          return <div key={book}>{book}</div>
+        }
+      })}
+      <div>{loading && 'Loading...'}</div>
+      <div>{error && 'Error'}</div>
     </>
-  );
+  )
 }
-
-export default App;
