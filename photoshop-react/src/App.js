77d57dad0baadb2b87779a13659750d3ac3dd5f2
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2023-06-17 06:39:36 +0200

    Add files via upload

---
+++ photoshop-react/src/App.js
@@ -0,0 +1,18 @@
+import React , { useState } from 'react'
+import './App.css'
+import Slider from './Slider'
+
+function App() {
+
+  return (
+      <div className="container">
+        <div className="main-image" />
+        <div className="sidebar">
+          sidebar
+        </div>
+        <Slider />
+      </div>
+  );
+}
+
+export default App;
