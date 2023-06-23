d39746fc8e46163c6a891b817a1a7ef535cc5474
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2023-06-23 06:29:31 +0200

    Update AllUsers.jsx

--- crud-with-react/src/components/AllUsers.jsx
+++ crud-with-react/src/components/AllUsers.jsx
@@ -1,6 +1,7 @@
-import { Table, TableBody, TableCell, TableHead, TableRow,styled } from '@mui/material'
+import { Button, Table, TableBody, TableCell, TableHead, TableRow,styled } from '@mui/material'
 import React, {useState,useEffect} from 'react'
-import { getUsers } from '../service/api'
+import { getUsers,deleteUser } from '../service/api'
+import { Link } from 'react-router-dom'
 
 
 // some styles
@@ -8,8 +9,17 @@ const StyledTable = styled(Table)`
   width: 90%;
   margin: 3rem auto;
 `
-const Thead = styled(TableRow)`
+const Trow = styled(TableRow)`
   background: #000;
+  & > th {
+    color: #fff;
+    font-size: 1.2rem;
+  }
+`
+const Tbody = styled(TableRow)`
+  & > td {
+    font-size: 1.5rem;
+  }
 `
 
 const AllUsers = () => {
@@ -27,27 +37,37 @@ const AllUsers = () => {
    let response = await getUsers()
    setUsers(response.data)
   }
+  const deleteUserData = async (id)=>{
+    await deleteUser (id)
+    getUsersDetails()
+  }
 
+  // empty TableCell cuz of UI :)
   return (
     <StyledTable>
-      <Thead>
-        <TableRow>
+      <TableHead>
+        <Trow>
           <TableCell>Id</TableCell>
           <TableCell>Name</TableCell>
           <TableCell>Username</TableCell>
           <TableCell>Email</TableCell>
           <TableCell>Phone</TableCell>
-        </TableRow>
-      </Thead>
+          <TableCell></TableCell> 
+        </Trow>
+      </TableHead>
       <TableBody>
         {users.map(user =>{
-          return <TableRow key={user.id}>
+          return <Tbody key={user.id}>
             <TableCell >{user.id}</TableCell>
             <TableCell >{user.name}</TableCell>
             <TableCell >{user.username}</TableCell>
             <TableCell >{user.email}</TableCell>
             <TableCell >{user.email}</TableCell>
-          </TableRow>
+            <TableCell>
+              <Button variant='contained'style={{marginRight: '.75rem'}} LinkComponent={Link} to={`/edit/${user.id}`}>Edit</Button>
+              <Button variant='contained' color='secondary' onClick={()=>deleteUserData(user.id)}>Delete</Button>
+            </TableCell>
+          </Tbody>
         })}
       </TableBody>
     </StyledTable>
