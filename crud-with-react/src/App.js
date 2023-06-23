ab253e30c63ccf8d8bdeb37c7982b882f40063f0
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2023-06-23 06:29:45 +0200

    Update App.js

--- crud-with-react/src/App.js
+++ crud-with-react/src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar';
 import MainContent from './components/MainContent';
 import AllUsers from './components/AllUsers';
 import AddUser from './components/AddUser'
+import EditUser from './components/EditUser';
 
 //router for routing on site
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -10,15 +11,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 //css
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <NavBar/>
+        <NavBar />
         <Routes>
           <Route path='/' element={<MainContent/>}/>
           <Route path='all' element={<AllUsers/>}/>
           <Route path='add' element={<AddUser/>}/>
+          <Route path='/edit/:id' element={<EditUser/>}/>
         </Routes>
       </BrowserRouter>  
     </div> 
