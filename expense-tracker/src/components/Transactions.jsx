baf5b3764f98d9cc0f8f43e5f6c36d1073c9e337
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2023-06-29 06:18:51 +0200

    Create Transactions.jsx

---
+++ expense-tracker/src/components/Transactions.jsx
@@ -0,0 +1,21 @@
+import { Box, Divider, List,Typography } from '@mui/material'
+import React from 'react'
+import Transaction from './Transaction'
+
+const Transactions = ({transactions}) => {
+  return (
+    <Box>
+        <Typography variant='h5'>Transactions History</Typography>
+        <Divider/>
+        <List>
+            {
+              transactions.map(transaction =>(
+                <Transaction transaction={transaction}/>
+              ))
+            }
+        </List>
+    </Box>
+  )
+}
+
+export default Transactions
