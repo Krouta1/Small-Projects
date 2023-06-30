b9311ff696432f7a6a9a741558732a3588ce8858
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2023-06-30 07:07:21 +0200

    Update Transaction.jsx

--- expense-tracker/src/components/Transaction.jsx
+++ expense-tracker/src/components/Transaction.jsx
@@ -1,16 +1,24 @@
 import styled from '@emotion/styled'
-import { ListItem, ListItemText } from '@mui/material'
+import { ListItem, ListItemIcon, ListItemText } from '@mui/material'
 import React from 'react'
+import { Delete } from '@mui/icons-material'
 
 const Detail = styled(ListItem)`
   margin-top:1rem;
 `
 
-const Transaction = ({transaction}) => {
+const Transaction = ({transaction,setTransactions,transactions}) => {
   const color= transaction.amount > 10 ? 'Green' : 'red'
+
+  const deleteTransaction = (id)=>{
+    setTransactions(transactions.filter(transaction=> transaction.id !==id))
+  }
 
   return (
     <Detail style={{backgroundColor:color, color: '#fff'}}>
+        <ListItemIcon>
+          <Delete onClick={()=>deleteTransaction(transaction.id)}/>
+        </ListItemIcon>
         <ListItemText>{transaction.text}</ListItemText>
         <ListItemText >{transaction.amount}</ListItemText>
     </Detail>
