ab7cfe31e1a59ee82996ea94d5517aad92bcdf2f
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2023-06-30 07:07:19 +0200

    Update NewTransactions.jsx

--- expense-tracker/src/components/NewTransactions.jsx
+++ expense-tracker/src/components/NewTransactions.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Box, Button, TextField, Typography } from '@mui/material'
 import styled from '@emotion/styled'
 
@@ -10,13 +10,25 @@ const Container = styled(Box)`
     }
 `
 
-const NewTransactions = () => {
+const NewTransactions = ({setTransactions}) => {
+  const [text, setText] = useState('')
+  const [amount, setAmount] = useState(0)
+
+  const addTranscaction = ()=>{
+    const transaction = {
+      id: Math.floor(Math.random() *1000),
+      text: text,
+      amount: +amount
+    }
+    setTransactions(prevState =>[transaction,...prevState,])
+  }
+
   return (
     <Container>
         <Typography variant='h5'>New transaction</Typography>
-        <TextField label='Enter expense'/>
-        <TextField label='Enter amount'/>
-        <Button variant='contained'>Add transaction</Button>
+        <TextField label='Enter expense' onChange={(e)=>setText(e.target.value)}/>
+        <TextField label='Enter amount' onChange={(e)=>setAmount(e.target.value)}/>
+        <Button variant='contained' onClick={()=> addTranscaction()}>Add transaction</Button>
     </Container>
   )
 }
